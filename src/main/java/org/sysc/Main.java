741b98c75df0becb2358e6d32ee078eaafdd5de6
lunacrina <astrearin@gmail.com>
2025-03-18 15:22:33 +0800

    -hi lun

--- src/main/java/org/sysc/Main.java
+++ src/main/java/org/sysc/Main.java
@@ -5,7 +5,8 @@ import org.sysc.Model.Student;
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(RegistrationForm::new);
-
+        //forked by rem
+        System.out.println("hello lun");
     }
 
 
