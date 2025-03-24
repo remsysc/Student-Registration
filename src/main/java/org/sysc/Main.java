a61e4e68852c6293236b0090c5d5655420d012fb
lizliz-loop <lizleocariza@gmail.com>
2025-03-24 16:31:17 +0800

    ayoko naa

--- src/main/java/org/sysc/Main.java
+++ src/main/java/org/sysc/Main.java
@@ -5,6 +5,7 @@ import org.sysc.Model.Student;
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(RegistrationForm::new);
+        System.out.printf("Hello World");
 
     }
 
