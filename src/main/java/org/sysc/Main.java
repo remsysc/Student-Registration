60c7d5bf918f5f9bbc5ac765f17320285d0f6087
Matigy <157974757+Matigy@users.noreply.github.com>
2025-03-25 09:26:33 +0800

    Fork Updated, resize window and title

--- src/main/java/org/sysc/Main.java
+++ src/main/java/org/sysc/Main.java
@@ -5,8 +5,5 @@ import org.sysc.Model.Student;
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(RegistrationForm::new);
-
     }
-
-
 }
